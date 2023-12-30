3edae35aade750f7874ddedd53aaf77f035021de
Rasmus Villebro <rasmus-villebro@hotmail.com>
2023-12-30 10:46:23 +0100

    fix errors

--- home/default.nix
+++ home/default.nix
@@ -14,17 +14,6 @@
     ./programs.nix
   ];
 
-  nixpkgs = {
-    overlays = [
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-    ];
-    config = {
-      allowUnfree = true;
-    };
-  };
-
   # enable home-manager
   programs.home-manager.enable = true;
 
