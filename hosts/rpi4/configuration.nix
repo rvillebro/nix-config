0a6628d0dca88f5ed2374bc78b12f736edefb512
Rasmus Villebro <rasmus-villebro@hotmail.com>
2024-12-15 21:17:43 +0100

    fix system packages for raspberry pi

--- hosts/rpi4/configuration.nix
+++ hosts/rpi4/configuration.nix
@@ -47,8 +47,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    hx
-    novim
+    neovim
     wget
     curl
     git
