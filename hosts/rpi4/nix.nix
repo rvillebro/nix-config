fcb6867104fe73ba5d16e60f75f9fa0353a7f246
Rasmus Villebro <rasmus-villebro@hotmail.com>
2024-12-16 16:57:51 +0100

    fix extra-substituters for rpi4

--- hosts/rpi4/nix.nix
+++ hosts/rpi4/nix.nix
@@ -50,11 +50,5 @@
     automatic = lib.mkDefault true;
     dates = lib.mkDefault "weekly";
     options = lib.mkDefault "--delete-older-than 7d";
-    extra-substituters = [ 
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 }
