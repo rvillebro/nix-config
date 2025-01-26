ad34fcbb85f9010e3157de83ca0b61890c9415dd
Rasmus Villebro <rasmus-villebro@hotmail.com>
2025-01-26 16:00:50 +0100

    add config to arguments

--- home/rpi4/default.nix
+++ home/rpi4/default.nix
@@ -1,6 +1,6 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{ pkgs, ... }: 
+{ pkgs, config, ... }: 
 {
   # You can import other home-manager modules here
   imports = [
