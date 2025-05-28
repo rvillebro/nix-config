3258067a00b46d6378b0291bc2cb22cb692224f0
Rasmus Villebro <rasmus-villebro@hotmail.com>
2025-05-28 21:51:24 +0200

    add rpi4 homeassistant virtualization

--- hosts/rpi4/configuration.nix
+++ hosts/rpi4/configuration.nix
@@ -53,5 +53,6 @@
     git
     sysstat
     neofetch
+    unstable.devenv
   ];
 }
--- hosts/rpi4/virtualisation.nix
+++ hosts/rpi4/virtualisation.nix
@@ -3,24 +3,30 @@
   virtualisation = {
     podman = {
       enable = true;
-      autoprune = true;
+      autoPrune.enable = true;
       dockerCompat = true;
       dockerSocket.enable = true;
     };
-    # oci-containers = {
-    #   backend = "podman";
-    #   containers.homeassistant = {
-    #     volumes = [ "home-assistant:/config" ];
-    #     environment.TZ = "Europe/Berlin";
-    #     # Note: The image will not be updated on rebuilds, unless the version label changes
-    #     image = "ghcr.io/home-assistant/home-assistant:stable";
-    #     extraOptions = [ 
-    #       # Use the host network namespace for all sockets
-    #       "--network=host"
-    #       # Pass devices into the container, so Home Assistant can discover and make use of them
-    #       "--device=/dev/ttyACM0:/dev/ttyACM0"
-    #     ];
-    #   };
-    # };
+    oci-containers = {
+      backend = "podman";
+      containers.homeassistant = {
+        volumes = [
+          "home-assistant:/config"
+          "/etc/localtime:/etc/localtime:ro"
+          "/run/dbus:/run/dbus:ro"
+        ];
+        # environment.TZ = "Europe/Copenhagen";
+        # Note: The image will not be updated on rebuilds, unless the version label changes
+        image = "ghcr.io/home-assistant/home-assistant:stable";
+        extraOptions = [ 
+          # Use the host network namespace for all sockets
+          "--network=host"
+          # Pass devices into the container, so Home Assistant can discover and make use of them
+          "--device=/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_ce0109eb7c4bef11be56c2a079f42d1b-if00-port0:/dev/ttyACM0"
+        ];
+      };
+    };
   };
+  networking.firewall.enable = false;
+  # networking.firewall.allowedTCPPorts = [ 8123 ];  # home assistant port
 }
